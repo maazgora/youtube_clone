e8e1fd0e0aa97727d3127e55b3de9ffd7600f0ea
Maaz Gora <maazgora@gmail.com>
2023-09-17 01:13:43 -0400

    Channel Detail Page

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -11,11 +11,13 @@ import GraphicEqIcon from '@mui/icons-material/GraphicEq';
 import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
 import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
 import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+import PsychologyIcon from '@mui/icons-material/Psychology';
 
 export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
 
 export const categories = [
   { name: 'New', icon: <HomeIcon />, },
+  { name: 'Think Media', icon: <PsychologyIcon />, },
   { name: 'Coding', icon: <CodeIcon />, },
   { name: 'ReactJS', icon: <CodeIcon />, },
   { name: 'NextJS', icon: <CodeIcon />, },
